135d3b13096bb5dd1f7c692e32abc8cdfdf2312e
Piotr Benetkiewicz <pbenetkiewicz@gmail.com>
2023-01-22 18:45:02 +0100

    initial commit

---
+++ FastlyApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿namespace FastlyApp.Controllers;
+
+using FastlyApp.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("{controller}")]
+public class HomeController : Controller
+{
+    [HttpGet]
+    [Route("foo")]
+    [CacheOnFastly(MaxAge = 60)]
+    public IActionResult Foo()
+    {
+        return Content($"Foo cached {DateTime.Now}");
+    }
+
+
+    [HttpGet]
+    [Route("bar")]
+    public IActionResult Bar()
+    {
+        return Content($"Bar {DateTime.Now}");
+    }
+}
---
+++ FastlyApp/Logic/CacheOnFastlyAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace FastlyApp.Logic;
+
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class CacheOnFastlyAttribute : ResultFilterAttribute
+{
+    public int MaxAge { get; set; }
+
+    public override void OnResultExecuting(ResultExecutingContext context)
+    {
+        context.HttpContext.Response.Headers.Add("Surrogate-Control", $"max-age={MaxAge}");
+        context.HttpContext.Response.Headers["Cache-Control"] = "no-store, max-age=0";
+        base.OnResultExecuting(context);
+    }
+}
